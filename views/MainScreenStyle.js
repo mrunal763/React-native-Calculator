f753ed4570f86e01395f53ea0f44238210f2a9f9
Mrunal Somankar <somankarmrunal763@gmail.com>
2024-12-26 22:35:13 +0530

    first commit

---
+++ views/MainScreenStyle.js
@@ -0,0 +1,114 @@
+module.exports = {
+    main_screen: {
+        marginTop: 30,
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: 'white',
+        width: '100%',
+        height: '80%',
+        alignItems: 'center',
+    },
+
+    // Display results
+    main_screen__display: {
+        elevation: 10,
+        width: '95%',
+        backgroundColor: 'white',
+        borderRadius: 10,
+        display: 'flex',
+        marginBottom: 10,
+        padding: 10,
+    },
+    main_screen__display_text: {
+        fontSize: 50,
+        textAlign: 'right',
+    },
+
+    main_screen__keypad: {
+        width: '100%',
+        height: '70%',
+        display: 'flex',
+    },
+    main_screen__keypad_row: {
+        display: 'flex',
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        width: '100%',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 10,
+    },
+    btn_outer: {
+        width: 75,
+        height: 75,
+        backgroundColor: 'white',
+        borderRadius: 90,
+        elevation: 10,
+        overflow: 'hidden',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    bg_button: {
+        backgroundColor: 'white',
+        color: 'black',
+        fontSize: 30,
+    },
+
+    btn1_outer: {
+        width: 75,
+        height: 75,
+        backgroundColor: '#FF5757',
+        borderRadius: 90,
+        elevation: 10,
+        overflow: 'hidden',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    bg1_button: {
+        backgroundColor: '#FF5757',
+        color: 'white',
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+
+    btn2_outer: {
+        width: 75,
+        height: 75,
+        backgroundColor: 'grey',
+        borderRadius: 90,
+        elevation: 10,
+        overflow: 'hidden',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    bg2_button: {
+        backgroundColor: 'grey',
+        color: 'white',
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+
+    btn3_outer: {
+        width: 75,
+        height: 75,
+        backgroundColor: 'green',
+        borderRadius: 90,
+        elevation: 10,
+        overflow: 'hidden',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    bg3_button: {
+        backgroundColor: 'green',
+        color: 'white',
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+
+    footer_text: {
+        marginTop: 10,
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+};
